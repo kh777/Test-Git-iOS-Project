757c615bfae5504399664c56081b1a2a1e084f3f
Koping Hu <kh777@yahoo.com>
2013-10-17 17:54:23 -0400

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by KP on 10/17/13.
+//  Copyright (c) 2013 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
